fd27eaa57d581c3b27be764364de690d0488194d
VSay29 <162121688+VSay29@users.noreply.github.com>
2025-10-08 11:27:46 +0200

    commit

--- app/src/main/java/com/example/kotlintestapp/Empleado.kt
+++ app/src/main/java/com/example/kotlintestapp/Empleado.kt
@@ -2,8 +2,8 @@ package com.example.kotlintestapp
 
 open class Empleado(val nombre: String, var sueldo: Double) {
 
-    open fun mostrarDetalles() {
-
+    open fun mostrarDetalles(): String {
+        return "Nombre: $nombre\nSueldo: $sueldo"
     }
 
 }
